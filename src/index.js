b22238038331787032dc1662bc91c6a725699f3b
Leo Dutra <leodutra.br@gmail.com>
2019-08-22 11:42:14 -0300

    Formats source

--- src/index.js
+++ src/index.js
@@ -69,12 +69,12 @@ function matchPattern (str, regexp) {
     const regexpClone = new RegExp(
         regexp.source,
         regexp.flags ||
-        (regexp.global ? 'g' : '') +
-        (regexp.ignoreCase ? 'i' : '') +
-        (regexp.multiline ? 'm' : '') +
-        (regexp.dotAll ? 's' : '') +
-        (regexp.unicode ? 'u' : '') +
-        (regexp.sticky ? 'y' : '')
+            (regexp.global ? 'g' : '') +
+            (regexp.ignoreCase ? 'i' : '') +
+            (regexp.multiline ? 'm' : '') +
+            (regexp.dotAll ? 's' : '') +
+            (regexp.unicode ? 'u' : '') +
+            (regexp.sticky ? 'y' : '')
     )
     regexpClone.lastIndex = 0
     const matches = []
