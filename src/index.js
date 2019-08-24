afb62e1e6928aaf69a160e5c6bc063180d87ed3e
Leo Dutra <leodutra.br@gmail.com>
2019-08-24 07:01:05 -0300

    Implements toImmutableProxy()

--- src/index.js
+++ src/index.js
@@ -248,6 +248,24 @@ function toUnicode (str) {
     return result
 }
 
+function toImmutableProxy (any) {
+    switch (typeof any) {
+    case 'object':
+    case 'function':
+    case 'xml':
+        return new Proxy(any, {
+            set: function immutableProxySet (target, prop) {
+                throw new Error('Cannot set property "' + prop + '", this object is immutable.')
+            },
+
+            get: function immutableProxyGet (target, prop) {
+                return toImmutableProxy(target[prop])
+            }
+        })
+    }
+    return any
+}
+
 module.exports = {
     baseX,
     bufferFromDataURI,
@@ -282,6 +300,7 @@ module.exports = {
     requireEnvVar,
     stringify,
     toCharEntity,
+    toImmutableProxy,
     toUnicode,
     typify,
     writeFile
