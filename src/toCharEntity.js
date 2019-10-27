b915ea1e43b6ee78bea1536a61ba1790141c18b8
Leo Dutra <leodutra.br@gmail.com>
2019-10-27 02:31:22 -0300

    Faster toCharEntity()

--- src/toCharEntity.js
+++ src/toCharEntity.js
@@ -1,7 +1,14 @@
 module.exports = function toCharEntity (str, toHex) {
     let result = ''
-    for (let i = 0, l = str.length; i < l;) {
-        result += `&#${toHex ? 'x' + str.charCodeAt(i++).toString('16') : str.charCodeAt(i++)};`
+    let i = str.length
+    while(i--) {
+		result = '&#' +
+			(toHex
+				? 'x' + str.charCodeAt(i).toString('16')
+				: str.charCodeAt(i)
+			) + 
+			';' +
+			result 
     }
     return result
 }
