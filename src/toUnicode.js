fdabee222737632f99dc2cfe7648a6e096b943cd
Leo Dutra <leodutra.br@gmail.com>
2019-10-27 02:10:26 -0300

    Fix toUnicode()

--- src/toUnicode.js
+++ src/toUnicode.js
@@ -2,7 +2,7 @@ module.exports = function toUnicode (str) {
     let result = ''
     let i = str.length
     while (i--) {
-        result += '\\u' + ('000' + str.charCodeAt(i).toString(16)).substr(-4)
+        result = '\\u' + ('000' + str.charCodeAt(i).toString(16)).substr(-4) + result
     }
     return result
 }
