cd031c0166389cec1a64c7924bc22aca7b58cf03
Leo Dutra <leodutra.br@gmail.com>
2019-12-02 23:15:29 -0300

    partial migration to TS

--- src/genBase62Uuidv1.ts
+++ src/genBase62Uuidv1.ts
@@ -3,6 +3,6 @@ import baseX from './baseX'
 
 const base62 = baseX('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-export default function genBase62Uuidv1 () {
+export default function genBase62Uuidv1() {
     return base62.encode(uuid.v1(null, Buffer.alloc(16)))
 }
--- src/genBase62Uuidv4.ts
+++ src/genBase62Uuidv4.ts
@@ -3,6 +3,6 @@ import baseX from './baseX'
 
 const base62 = baseX('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-export default function genBase62Uuidv4 () {
+export default function genBase62Uuidv4() {
     return base62.encode(uuid.v4(null, Buffer.alloc(16)))
 }
