c6e93c2c60a0c0c1232578892aa6bc97cb903329
Leonardo Dutra <leodutra.br@gmail.com>
2021-04-18 19:45:51 -0300

    Update typify.js

--- src/typify.js
+++ src/typify.js
@@ -1,16 +1,19 @@
 const getClassName = require('./getClassName')
 
-module.exports = function typify (any, ownProperties) {
-    const t = getClassName(any)
-    if (t === 'Object') {
-        const result = {}
-        for (const prop in any) {
-            if (ownProperties && !Object.prototype.hasOwnProperty.call(any, prop)) {
-                continue
-            }
-            result[prop] = getClassName(any[prop])
-        }
-        return result
-    }
-    return t
+module.exports = function typify (any, { ownProperties = false, expandObj = true } = {}) {
+  const t = getClassName(any)
+  if (t === 'Object') {
+      const result = {}
+      for (const prop in any) {
+          if (ownProperties && !Object.prototype.hasOwnProperty.call(any, prop)) {
+              continue
+          }
+          const className = getClassName(any[prop])
+          result[prop] = expandObj && className === 'Object'
+            ? typify(any[prop], ownProperties)
+            : className
+      }
+      return result
+  }
+  return t
 }
